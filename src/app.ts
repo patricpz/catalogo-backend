ca2d346296151bb9d0c21235dadb0f2a39e52029
patricpz <patricbarros19@gmail.com>
2026-04-24 18:16:13 -0300

    feat:cors

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import { env } from "./config/env.js";
 import { apiRouter } from "./routes/index.js";
 import { errorMiddleware } from "./middlewares/error.middleware.js";
@@ -6,8 +7,15 @@ import { errorMiddleware } from "./middlewares/error.middleware.js";
 export function createApp() {
   const app = express();
 
+  app.use(
+    cors({
+      origin: env.corsOrigin,
+      credentials: true,
+      methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+      allowedHeaders: ["Content-Type", "Authorization"],
+    })
+  );
   app.use(express.json({ limit: "1mb" }));
-
 
   app.use("/api", apiRouter);
 
